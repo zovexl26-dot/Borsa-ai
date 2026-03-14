24dc7e1d1ce6859923aa0dd015b8e9f0122e4596
zovexl26-dot <zovexl26@gmail.com>
2026-03-14 21:22:11 +0300

    port fix

--- server/index.js
+++ server/index.js
@@ -441,7 +441,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Borsa AI çalışıyor: http://localhost:${PORT}`);
 });
